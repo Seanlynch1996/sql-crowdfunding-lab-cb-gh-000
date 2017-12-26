05455f723fce23a8e5a7942db9e32fb3fd503ad5
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2017-12-26 18:34:42 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -20,13 +20,13 @@ INSERT INTO users (name, age) VALUES ("A",5),
 ("T",100);
 
 INSERT INTO projects(title, category, funding_goal, start_date, end_date) VALUES
-"Project1","FooA",1000,"24","389"),
-"Project2","FooB",2000,"24114","24479"),
-"Project3","FooC",4000,"48569","48934"),
-"Project4","FooD",8000,"73389","73754"),
-"Project5","FooE",16000,"98574","98939"),
-"Project6","FooF",32000,"124124","124489"),
-"Project7","FooG",64000,"150039","150404"),
-"Project8","FooH",128000,"176319","176684"),
-"Project9","FooI",256000,"202964","203329"),
-"Project10","FooJ",512000,"229974","230339");
+("Project1","FooA",1000,"24","389"),
+("Project2","FooB",2000,"24114","24479"),
+("Project3","FooC",4000,"48569","48934"),
+("Project4","FooD",8000,"73389","73754"),
+("Project5","FooE",16000,"98574","98939"),
+("Project6","FooF",32000,"124124","124489"),
+("Project7","FooG",64000,"150039","150404"),
+("Project8","FooH",128000,"176319","176684"),
+("Project9","FooI",256000,"202964","203329"),
+("Project10","FooJ",512000,"229974","230339");
